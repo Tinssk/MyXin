7e0f36ef6c6961995abd9d5d1438e8d26332f197
name <email@example.com>
2025-01-07 23:11:26 +0800

    tt

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,20 @@
-// service-worker.js
-self.addEventListener("install", (event) => {
+// Updated version: 2025-01-07
+this.addEventListener("install", (event) => {
+  console.log("zz");
   event.waitUntil(
-    caches.open("my-cache").then((cache) => {
+    caches.open("my-cache1").then((cache) => {
+      console.log("yy");
       return cache.addAll([
-        "clickAni.gif", // 缓存 GIF 文件
+        "./clickAni.gif", // 缓存 GIF 文件
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response; // 从缓存中返回 GIF
-      }
-      return fetch(event.request); // 如果缓存没有，去请求网络
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
     })
   );
 });
